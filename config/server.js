039e8129d9e0dab3db29f8425c87a9c3995b0a2b
luxiancan <18707733672@163.com>
2020-10-19 08:35:47 +0800

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1f0bda9c23e03d31a94826f493391a96'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6baea56d-ef75-4522-ad8c-6ca6f54022b1'
+};
